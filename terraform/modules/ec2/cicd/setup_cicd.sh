6c9a3cd9a6a670f35c98a0d134f8f2d5c172bc80
Tran Kien Quoc <Kienquoctran08@gmail.com>
2024-12-08 20:24:33 +0700

    Update setup_cicd.sh

--- terraform/modules/ec2/cicd/setup_cicd.sh
+++ terraform/modules/ec2/cicd/setup_cicd.sh
@@ -52,11 +52,11 @@ sudo kubectl version
 # [ArgoCD] Step 2:
 # - Install and Run ArgoCD K8S.
 # - Update the "argocd-server" service type to LoadBalancer.
-# - Sleep 70 seconds to ensure all ArgoCD pods are ready before going to next steps.
+# - Sleep 80 seconds to ensure all ArgoCD pods are ready before going to next steps.
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-sudo sleep 70s
+sudo sleep 80s
 
 # [ArgoCD] Step 3: Create the systemd service file for ArgoCD K8S Port-Forward
 # - Format: Host_Port -> K8S_Service_Port
